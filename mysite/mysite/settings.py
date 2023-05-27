bf961ea96962e26a262a219f0382ee6435971bc6
AltruistDevs <127851912+AltruistDevs@users.noreply.github.com>
2023-05-27 20:19:14 +0100

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,9 +85,11 @@ else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
+        'NAME': env('PCNAME'),
+        'USER': env('PCUSER'),
+        'PASSWORD': env('PCPASSWORD'),
+        'HOST': env('PCHOST'),
+        'PORT': env('PCPORT'),
     }
 }
 
