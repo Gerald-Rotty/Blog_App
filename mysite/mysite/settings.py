4fe2b40951d3afee8fc0bf27727b5c4f419292d5
Gerald-Rotty <rotgakgoyit80@gmail.com>
2023-05-29 11:41:38 +0100

    static updated

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 import environ
 env = environ.Env()
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
